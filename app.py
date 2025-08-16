d6519eb64ff22a39ca7c2eba9d9aef9cfab182ab
Harr1106 <hwu061361@gmail.com>
2025-08-16 21:52:55 +1200

    Updating python file

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/stillDeveloping.html")
+def home():
+    return render_template("stillDeveloping.html")
+
 if __name__ == "__main__":
     app.run()
