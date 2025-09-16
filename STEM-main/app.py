15445efc915154104a9a7c4a9a19ef9285cfc85f
HarryW808 <harrwu888@gmail.com>
2025-09-16 18:39:53 +1200

    Add files via upload

---
+++ STEM-main/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/stillDeveloping.html")
+def still_developing():
+    return render_template("stillDeveloping.html")
+    
+@app.route("/origami")
+def origami():
+    return render_template("origami.html")
+
+@app.route("/legospikes")
+def legospikes():
+    return render_template("legospikes.html")
+    
+if __name__ == "__main__":
+    app.run()
